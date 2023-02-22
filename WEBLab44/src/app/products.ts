0ea71dd91df5d12933142de70984a3fb7f3b670e
Dilyaaaa <124158228+Dilyaaaa@users.noreply.github.com>
2023-02-22 17:18:22 +0600

    I have added one product

--- WEBLab44/src/app/products.ts
+++ WEBLab44/src/app/products.ts
@@ -1,22 +1,29 @@
 export interface Product {
   id: number;
+  image: string;
   name: string;
   price: number;
   description: string;
+  rating: number;
+  url: string;
 }
 
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    image: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h45/h27/64487158087710/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg",
+    name: 'Xiaomi Redmi Note 10 Pro',
+    price: 128350,
+    description: ' NFC technology: Yes Color: Gray Screen type: Color AMOLED, Touch diagonal: 6.67 inch Battery capacity: 5020 mAh',
+    url: "https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000#!/item",
   },
   {
     id: 2,
+    image:" ",
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    url:"",
   },
   {
     id: 3,
