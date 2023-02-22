a1094dce4fd56bfd09344381f39b133f29915abc
Dilyaaaa <124158228+Dilyaaaa@users.noreply.github.com>
2023-02-22 18:01:46 +0600

    Added rating

--- WEBLab44/src/app/products.ts
+++ WEBLab44/src/app/products.ts
@@ -15,6 +15,7 @@ export const products = [
     name: 'Xiaomi Redmi Note 10 Pro',
     price: 128350,
     description: ' NFC technology: Yes Color: Gray Screen type: Color AMOLED, Touch diagonal: 6.67 inch Battery capacity: 5020 mAh',
+    rating: 7,
     url: "https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000#!/item",
   },
   {
@@ -23,6 +24,7 @@ export const products = [
     name: 'Блендер Fresh Juice Blend12 белый',
     price: 12000 ,
     description: 'тип: портативный мощность: 50 Вт управление: электронное количество скоростей: 1 цвет: белый, ,прозрачный',
+    rating: 7.2,
     url:"https://kaspi.kz/shop/p/fresh-juice-blend12-belyi-105722831/?c=750000000#!/item",
   },
   {
@@ -31,6 +33,7 @@ export const products = [
     name: 'Блендер ARNICA Diva Trend черный, розовый',
     price: 28740 ,
     description: 'тип: погружной мощность: 1200 Вт управление: механическое количество скоростей: 2 цвет: черный, ,розовый',
+    rating: 5,
     url:"https://kaspi.kz/shop/p/arnica-diva-trend-chernyi-rozovyi-100701067/?c=750000000#!/item",
   },
   {
@@ -39,6 +42,7 @@ export const products = [
     name:'Телевизор Yasin LED-32E7000 81 см черный',
     price: 60215 ,
     description:'поддержка HD: 720p HD технология Smart TV: Нет  wi-Fi: Да входы: AV, ,HDMI, ,USB, ,антенный, ,Bluetooth',
+    rating: 8,
     url:"https://kaspi.kz/shop/p/yasin-led-32e7000-81-sm-chernyi-103411518/?c=750000000#!/item",
   },
   {
@@ -47,6 +51,7 @@ export const products = [
     name:'Телевизор Yasin 40G7 102 см черный',
     price: 114677,
     description:'технология Smart TV: Да wi-Fi: Да входы: оптический, ,AV, ,компонентный, ,HDMI, ,Ethernet (RJ-45), ,USB, ,RS-232, ,MHL',
+    rating: 9,
     url:"https://kaspi.kz/shop/p/yasin-40g7-102-sm-chernyi-104045834/?c=750000000#!/item",
   },
   {
@@ -55,6 +60,7 @@ export const products = [
     name:'Игровой центр LEMENGKEKU Логарифмическая доска мультиколор',
     price: 3300,
     description:'размер, см: 45х18 см пол ребенка: универсальный цвет: мультиколор основной материал: дерево',
+    rating: 9,
     url:"https://kaspi.kz/shop/p/igrovoi-tsentr-lemengkeku-logarifmicheskaja-doska-mul-tikolor-102413320/?c=750000000#!/item",
   },
   {
@@ -63,6 +69,7 @@ export const products = [
     name:'Компьютерные колонки SVEN MS-304 черный',
     price: 29480,
     description:'тип: 2.1 суммарная мощность: 40 Вт материал: MDF цвет: черный',
+    rating: 8,
     url:"https://kaspi.kz/shop/p/sven-ms-304-chernyi-11600448/?c=750000000#!/item",
   },
   {
@@ -71,6 +78,7 @@ export const products = [
     name:'Домашний кинотеатр LG LHB655NK',
     price: 194958,
     description:'основной блок: одноблочная система режим караоке: караоке поддерживаемые носители: BD (Blu-ray Disc), ,Blu-ray 3D, ,BD-R, ,BD-Re, ,CD, ,CD-R, ,CD-RW, ,DVD, ,DVD R, ,DVD RW',
+    rating: 7,
     url:"https://kaspi.kz/shop/p/lg-lhb655nk-2100099/?c=750000000#!/item",
   },
   {
@@ -79,6 +87,7 @@ export const products = [
     name:'Переходник ViTi HDMI - VGA HDV600',
     price: 1348,
     description:'тип: переходник разъемы: HDMI - VGA',
+    rating: 10,
     url:"https://kaspi.kz/shop/p/perehodnik-viti-hdmi---vga-hdv600-101519581/?c=750000000#!/item",
   },
   {
@@ -87,6 +96,7 @@ export const products = [
     name:'Кабель Ugreen DisplayPort - DisplayPort 10211 2м черный',
     price: 4000,
     description:'тип: кабель разъемы: DisplayPort - DisplayPort длина кабеля: 2 м',
+    rating: 7,
     url:"https://kaspi.kz/shop/p/kabel-ugreen-displayport---displayport-10211-2m-chernyi-101207849/?c=750000000#!/item",
   }
 
