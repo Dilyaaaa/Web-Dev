a05ae2afbcfe8b7953e2d9f50ae78af7b3d12f14
Dilyaaaa <124158228+Dilyaaaa@users.noreply.github.com>
2023-04-05 16:01:23 +0600

    Created api

---
+++ WEBLab8/src/app/product-item/product-item.component.ts
@@ -0,0 +1,40 @@
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+import { products,Product } from '../products';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent {
+  products = [...products];
+@Input( )selectedProduct:string |undefined;
+
+like(products:Product){
+  products.like+=1;
+}
+
+remove(product:Product){
+  for(let i=0; i <products.length;i++){
+    if(products[i]==product){
+      delete products[i];
+      break;
+    }
+  }
+//   for(let i=0; i <products.length;i++){
+//        if(products[i].id==product.id){
+//         products.splice(i,1);
+//          break;
+//        }
+//      }
+}
+  share() {
+  }
+  kaspi(a:string|URL|undefined){
+    window.open("https://kaspi.kz/shop/search")
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
